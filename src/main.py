0eab37445971dd2ddbeaaa381a18b19eeee07f15
Yannis Van Achter <yannis.van.achter@gmail.com>
2023-03-31 00:22:41 +0200

    Update main.py
    Add connect and disconnect funciton
    
    Co-Authored-By: Youlan <57191896+iDraKzy@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,17 +1,34 @@
 # encoding uft-8
-import mysql.connector
+
+# import
+# native
 from time import sleep
 
-def main():
-    print("Hello, world!")
-    print("Helle Aline :)")
-    print("Hello Loulou")
-    sleep(3)
-    db = mysql.connector.connect(user='root', passwd='password', host='mysql', database='mysql')
-    print(db.is_connected())
+# pip install -r requerements.txt
+import mysql.connector
 
+# local modules
+from exception.unconnectederror import UnConnectedError
+
+def db_connect(user, password, host, database):
+    # TODO: specify + place type hinting
+    db = mysql.connector.connect(user=user, passwd=password, database=database, host=host)
+    if not db.is_connected():
+        raise UnConnectedError()
+    
     cursor = db.cursor()
+        
+    return db, cursor
 
+def db_disconnect(db, cursor):
+    # TODO: specify + place type hinting
+    cursor.close()
+    db.disconnect(cursor)
+
+def main():
+    db, cursor = db_connect(user='root', passwd='password', host='mysql', database='mysql')
+
+    # TODO: remove until db_disconnect()
     cursor.execute("CREATE TABLE IF NOT EXISTS test(id INTEGER(64) PRIMARY KEY, name VARCHAR(255))")
 
     cursor.execute("INSERT INTO test VALUES (2, 'bla')")
@@ -21,6 +38,8 @@ def main():
 
     for row in cursor.fetchall():
         print(row)
+        
+    db_disconnect(db, cursor)
 
 if __name__ == "__main__":
     main()
