ad9f13093caea5b9f1d5bf620405675337e8ad02
Yannis Van Achter <yannis.van.achter@gmail.com>
2023-03-31 09:16:33 +0200

    fix docker-compose and update main.py
    no more error in python app on start
    
    Co-Authored-By: Youlan <57191896+iDraKzy@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -2,17 +2,46 @@
 
 # import
 # native
-from time import sleep
+pass
+import time
 
 # pip install -r requerements.txt
-import mysql.connector
+import mysql.connector as mysql
+from mysql.connector.connection import MySQLConnection
+from mysql.connector import CMySQLConnection
+from mysql.connector.cursor_cext import CMySQLCursor
+
 
 # local modules
 from exception.unconnectederror import UnConnectedError
 
-def db_connect(user, password, host, database):
-    # TODO: specify + place type hinting
-    db = mysql.connector.connect(user=user, passwd=password, database=database, host=host)
+def db_connect(user: str, password: str, host: str|int, database: str) -> (tuple[MySQLConnection|CMySQLConnection, CMySQLCursor]):
+    """Connect to database and place cursor
+
+    Args:
+    -----
+        user (str): user name to connect to database
+        password (str): password to connect to database
+        host (str|int): host to connect by container network in Docker
+        database (str): database name to connect
+
+    Raises:
+    ------
+        UnConnectedError: If database is not connected
+
+    Returns:
+    --------
+        tuple (MySQLConnection|CMySQLConnection, CMySQLCursor): db, cursor
+        
+    Version:
+    --------
+        1.0.0
+        
+    Author:
+    -------
+        Youlan Collart & Yannis Van Achter
+    """
+    db = mysql.connect(user=user, passwd=password, database=database, host=host)
     if not db.is_connected():
         raise UnConnectedError()
     
@@ -20,15 +49,30 @@ def db_connect(user, password, host, database):
         
     return db, cursor
 
-def db_disconnect(db, cursor):
-    # TODO: specify + place type hinting
+def db_disconnect(db: MySQLConnection|CMySQLConnection, cursor: CMySQLCursor) -> (None):
+    """Disconnect to database and close cursor
+
+    Args:
+    -----
+        db (MySQLConnection|CMySQLConnection): database instance
+        cursor (CMySQLCursor): cursor instance
+        
+    Version:
+    --------
+        1.0.0
+        
+    Author:
+    -------
+        Youlan Collart & Yannis Van Achter
+    """
     cursor.close()
-    db.disconnect(cursor)
+    db.disconnect()
 
 def main():
-    db, cursor = db_connect(user='root', passwd='password', host='mysql', database='mysql')
+    time.sleep(3)
+    db, cursor = db_connect(user='user', password='password', host='mysql', database='mysql')
 
-    # TODO: remove until db_disconnect()
+    # TODO: create test for db_disconnect() and db_connect() with it
     cursor.execute("CREATE TABLE IF NOT EXISTS test(id INTEGER(64) PRIMARY KEY, name VARCHAR(255))")
 
     cursor.execute("INSERT INTO test VALUES (2, 'bla')")
