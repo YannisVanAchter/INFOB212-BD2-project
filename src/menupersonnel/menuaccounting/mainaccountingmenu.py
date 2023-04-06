043aaa7afebe87e461e857a717650a2fef895e80
Yannis Van Achter <yannis.van.achter@gmail.com>
2023-04-06 15:57:21 +0200

    Update mainaccountingmenu.py
    Specification of accounting menu

--- src/menupersonnel/menuaccounting/mainaccountingmenu.py
+++ src/menupersonnel/menuaccounting/mainaccountingmenu.py
@@ -1,7 +1,26 @@
 # encoding uft-8
 
-from module.get import get_string, get_float, get_int
+from module.get import get_string, get_float, get_int, get_sql_user_querry
 from module.database import DataBase
+from module.utils import clear_terminal as cls
 
 def main_accounting_menu(database: DataBase):
-    print("In compta menu")
+    """Accountent menu
+
+    allow user to:
+    --------------
+        - set product price
+        - update price
+        - insert new product
+        - research for accounting stuff
+            - selling quantity
+            - where are the clients
+            - get price of each command
+            - get/set price of delivery
+
+    Args:
+    -----
+        database (DataBase): Data base connected for this user (the accountent)
+    """
+    cls()
+    
