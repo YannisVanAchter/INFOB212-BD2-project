856bedae74cc6bffbccd9913e10d3cd778f53a34
AurelieGnt <99643639+AurelieGnt@users.noreply.github.com>
2023-04-12 17:19:43 +0200

    Commencement de la vue RH

--- src/menupersonnel/RH/mainRHmenu.py
+++ src/menupersonnel/RH/mainRHmenu.py
@@ -1,4 +1,17 @@
 from module.database import DataBase
 
 def main_RH_menu(database: DataBase):
+    """
+    Menu for HR. 
+    
+    allow the person of HR to: 
+    --------------------------
+    - Add employee
+    - Modify employee
+    - Delete employee 
+
+    Args:
+        database (DataBase): Data base connected for HR 
+    """
+   
     print("In RH menu")
