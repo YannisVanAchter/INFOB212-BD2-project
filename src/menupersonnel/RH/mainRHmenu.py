79c2eeabf3abf3b62dd9a10977009eb7810c633e
AurelieGnt <99643639+AurelieGnt@users.noreply.github.com>
2023-04-14 17:14:16 +0200

    Commencement du menu RH
    Menu principal qui demande quelle action le membre des RH souhaite accomplir
    3 choix : add - modify- delete un employée

--- src/menupersonnel/RH/mainRHmenu.py
+++ src/menupersonnel/RH/mainRHmenu.py
@@ -1,4 +1,7 @@
 from module.database import DataBase
+from module.get import get_int 
+from module.get import get_bool
+from module.get import get_string 
 
 def main_RH_menu(database: DataBase):
     """
@@ -15,3 +18,50 @@ def main_RH_menu(database: DataBase):
     """
    
     print("In RH menu")
+    validity = False
+
+    while (validity == False): 
+        print("Choose what you want to do: ")
+        print("Type 0 if you want add an employee to the company ")
+        print("Type 1 if you want to modify an actual employee ")
+        print("Type 2 if you want to delete an actual employee ")
+        choice = get_int("your choice: ")
+        if (choice != 0 and choice != 1 and choice != 2):
+            validity = False 
+            print("This operation is not possible, please choose another number")
+        else: 
+            validity = True 
+    
+    # According to the number choosen, will display the good choice
+    
+    if choice == 0:
+        add_employee()
+    
+    if choice == 1: 
+        modify_employee()
+        
+    if choice == 2: 
+        delete_employee()
+
+
+def add_employee():
+    """ 
+    To add a new employee
+    """
+    DataBase.connect()
+    print("You have choosen to add a new employee")
+    existence = get_bool("Does this new employee is already register in our system ? Enter 0 if no and 1 if yes ")
+    
+    DataBase.disconnect()
+    
+def modify_employee(): 
+    """_
+    To modify an actual employee (to change his function)
+    """
+    print("Modify bien appelé")
+    
+def delete_employee(): 
+    """
+    To delete an employee of the 
+    """
+    print("delete bien appelé")
