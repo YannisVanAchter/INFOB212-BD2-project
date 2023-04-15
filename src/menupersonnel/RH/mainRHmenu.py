d3b0fed2825e0fd655c1c549b04d45b132087b1d
AurelieGnt <99643639+AurelieGnt@users.noreply.github.com>
2023-04-15 15:56:10 +0200

    Update mainRHmenu.py
    Option ajouter un new employée
    Option modifier un employée
    Début de supprimer un employée

--- src/menupersonnel/RH/mainRHmenu.py
+++ src/menupersonnel/RH/mainRHmenu.py
@@ -4,7 +4,7 @@ from module.get import get_bool
 from module.get import get_string 
 from menuconnexion.menu import main_login_menu
 
-def main_RH_menu(database: DataBase):
+def main_RH_menu(db: DataBase):
     """
     Menu for HR. 
     
@@ -15,11 +15,12 @@ def main_RH_menu(database: DataBase):
     - Delete employee 
 
     Args:
-        database (DataBase): Data base connected for HR 
+        db (DataBase): Data base connected for HR 
     """
    
     print("You are now in HR menu")
     validity = False
+    
 
     while (validity == False): 
         print("Choose what you want to do: ")
@@ -32,7 +33,7 @@ def main_RH_menu(database: DataBase):
             print("This operation is not possible, please choose another number")
         else: 
             validity = True 
-    
+    db.connect()
     # According to the number choosen, will redirect to the good function
     if choice == 0:
         add_employee()
@@ -42,66 +43,135 @@ def main_RH_menu(database: DataBase):
         
     if choice == 2: 
         delete_employee()
+    db.disconnect()
 
 
-def add_employee():
+def add_employee(db : DataBase):
     """ 
     To add a new employee
+    Use the function create_person() and the function create_employee(id)
+    
+    Args:
+    db (DataBase): Data base connected for HR 
     """
-    DataBase.connect()
+    
+    db.connect()
     print("You have choosen to add a new employee")
     existence = get_bool("Does this new employee is already register in our system ? Enter False if no and True if yes ")
+    
     if existence == True: 
         create_person() 
     
     else : 
         id_person = get_int("Please enter the id of the person: ")
-        if id_person not in (SELECT id FROM PERSON): ## A revoir
-            print("This person doesn't exist")
-        else: 
-            create_employee(id)
+        create_employee(id_person)
         
-    DataBase.disconnect()
+    db.disconnect()
     
 def create_person(): 
     """
     To add a new employee who hasn't already created an account in the company. 
     Create a new account and call the function create_employee(id) with the id created
     """
-    main_login_menu(database: DataBase) ## Appelle la fonction de Youlan pr la création d'un compte 
+    main_login_menu(DataBase) ## Appelle la fonction de Youlan pr la création d'un compte 
     ## On crée nous-même un compte employée mais du coup en théorie on génère ici un mdp temporaire que la personne devra changer 
     id_person = get_int("Please enter the id of the person: ")
     create_employee(id_person)
 
 
-def create_employee(id): 
+def create_employee(id, db: DataBase): 
     """
     To add to a person who has already created an account in the company the role of employee 
     
     Args: 
     id : the id of the person that has already had an account in the company 
+    db (DataBase): Data base connected for HR 
     """
+    db.connect()
+    
     print("Now you will give the job of the user with this id")
     
-            id_transplantation = input("Pouvez vous me donner l'identifiant de la transplantation dont vous souhaitez voir les organes?")
-
-        # use 3*" your querry 3*", that will provide only one string
-        #
-        # Example:
-        # organes = ("""SELECT etat, methode_de_conservation, type FROM ORGANE WHERE id_organe in
-        # (SELECT id_organe FROM TRANSPLANTATION WHERE id_transplantation = %s)""")
-        organes = ("SELECT etat, methode_de_conservation, type FROM ORGANE WHERE id_organe in"
-        "(SELECT id_organe FROM TRANSPLANTATION WHERE id_transplantation = %s)")
-        database.execute(organes, (id_transplantation))
+    people = ("SELECT id from PERSON")
     
-def modify_employee(): 
+    if id not in people: 
+        print('The id that you entered is not valid')
+    
+    salary_person = get_int("Please enter the salary of the person: ")
+    function_person = get_string("Please enter the exact function of the person in the company: ")
+        
+    db.execute("INSERT INTO STAFF (id, salary, function) VALUES ({id}, {salary_person}, {function_person})")
+    
+    print("Choose the category of the person: ")
+    print("Type 0 if the person is an anaesthetist")
+    print("Type 1 if the person is a nurse")
+    print("Type 2 if the person is a doctor")
+    print("Type 3 if the person is an accountant")
+    print("Type 4 if the person is a HR person")
+    print("Type 5 if the person is a CEO")
+    print("Type 6 if the person doesn't belong to a particular category")
+    
+    category = get_int("The number of the category is: ")
+    
+    if category not in {0,1,2,3,4,5,6} :
+        print("The number entered is not valid")
+    else: 
+        if category == 0:
+            inami = get_string("Enter the INAMI number of the person:")
+            db.execute("INSERT INTO ANAESTHETIST (id,inami_number) VALUES ({id},{inami})")
+        if category == 1: 
+            db.execute("INSERT INTO NURSE (id) VALUES ({id})")
+        if category == 2: 
+            inami = get_string("Enter the INAMI number of the person:")
+            db.execute("INSERT INTO DOCTOR (id,inami_number) VALUES ({id},{inami})")
+        if category == 3: 
+            db.execute("INSERT INTO ACCOUNTANT (id) VALUES ({id})")  
+        if category == 4: 
+            db.execute("INSERT INTO HR (id) VALUES ({id})")  
+        if category == 5: 
+            db.execute("INSERT INTO CEO (id) VALUES ({id})")  
+            
+    db.disconnect()
+            
+def modify_employee(db : DataBase): 
     """_
     To modify an actual employee (to change his function)
-    """
-    print("Modify bien appelé")
     
-def delete_employee(): 
+    Args:
+    db (DataBase): Data base connected for HR 
     """
-    To delete an employee of the 
+    db.connect()
+    id_employee = get_int("Enter the id of the employee")
+    if id_employee not in ("SELECT id FROM STAFF"):
+        print("The id of the person is not valid")
+    else: 
+        print("What do you want to do?")
+        choice = get_int("Type 1 if you want to modify the salary of the employee and 2 if you want to modify his function:")
+        if choice not in {1,2}:
+            print("Please enter a valid integer")
+        else: 
+            if choice == 1: 
+                new_salary = get_int("Enter the new salary: ")
+                db.execute("UPDATE salary FROM STAFF WHERE ID = id_employee and salary = new_salary") ##A revoir 
+            if choice == 2: 
+                new_function = get_string("Enter the new function: ")
+                db.execute("UPDATE function FROM STAFF WHERE ID = id_employee and function = new_function ")
+        
+    db.disconnect()
+    
+def delete_employee(db: DataBase): 
     """
-    print("delete bien appelé")
+    To delete an employee 
+    
+    Args: 
+    db (DataBase): Data base connected for HR 
+    """
+    db.connect()
+    id_employee = get_int("Enter the id of the employee")
+    
+    if id_employee not in ("SELECT id FROM STAFF"):
+        print("The id of the person is not valid")
+    else: 
+       print("Are you sure to delete this employee?")
+       
+       
+    ## LOOK HOW TO DELETE THE EMPLOYEE IN THE DB
