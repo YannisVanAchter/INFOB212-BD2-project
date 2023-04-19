c115c6bc1421661ca10964c34f072e0adb5e2333
elineee <99394485+elineee@users.noreply.github.com>
2023-04-19 12:14:01 +0200

    Update __init__.py
    remarque pr yanis

--- src/menupersonnel/menuaccounting/view/__init__.py
+++ src/menupersonnel/menuaccounting/view/__init__.py
@@ -7,3 +7,11 @@ from .askquantity import ask_blood_quantity, ask_price
 from .askorganstate import ask_organ_state
 from .askorganconservationmethod import ask_organ_conservation_method
 from .askage import ask_age_of_death
+
+''' 
+voici mon commentaire pour toi yanis: y'a bcp trop de fichiers pr un simple menu: je trouve que faire un fichier python pr une fonction 
+ c'est beaucoup trop et ça m'a donné mal à la tête
+ Un SEUL fichier avec toutes tes fonctions ask et un fichier main serait bcp mieux et plus agréable car avec tous ces fichiers, on s'y retrouve plus!!
+ (signé Eline) 
+ 
+'''
