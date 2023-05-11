303b6bff4aa6031c2bc9815958c125fecf1e8ab0
iDraKzy <57191896+iDraKzy@users.noreply.github.com>
2023-05-11 11:03:01 +0200

    fix register returning None

--- src/auth/authenticate.py
+++ src/auth/authenticate.py
@@ -125,9 +125,7 @@ def register(
     query = f"INSERT INTO PERSON ({argsForQuery}) VALUES ({argsValueForQuery});"
     print(query)
 
-    db.execute_with_params(query, tuple(argsValue))
-
-    personId = db.last_row_id
+    personId = db.execute_with_params(query, tuple(argsValue))
 
     if registerCustomer != None:
         become_customer(db, personId, registerCustomer["nickname"], registerCustomer["bloodType"], registerCustomer["bloodSign"])
@@ -142,7 +140,7 @@ def become_customer(db: DataBase, personId: int, nickname: str, bloodType: str, 
     """
     db.execute_with_params("INSERT INTO CUSTOMER (id, pseudo, blood_type, blood_sign) VALUES (%s,%s,%s,%s)", (personId, nickname, bloodType, bloodSign))
 
-def remove_user(db: DataBase, userId: int) -> str | True:
+def remove_user(db: DataBase, userId: int) -> bool:
     """
     Remove a user from the database.
 
@@ -151,7 +149,7 @@ def remove_user(db: DataBase, userId: int) -> str | True:
     queryIsStaff = "SELECT id FROM STAFF WHERE id = %s;"
     db.execute_with_params(queryIsStaff, [userId])
     if len(db.tableArgs) > 0:
-        return "Can't delete your account when you're a staff member."
+        return False
     
     queryUpdate = "UPDATE PERSON SET last_name = 'Anonymized',first_name = 'Anonymized' email = 'Anonymized', phone_number = 'Anonymized', born_date = '1970-01-01', Liv_id = 1 WHERE id = %s;"
     db.execute_with_params(queryUpdate, [userId])
