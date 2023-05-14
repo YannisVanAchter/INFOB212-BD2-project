3ed705da0c69f53453f80e1e352199b5ead441b3
AlineBlg <aline.boulanger@student.unamur.be>
2023-05-14 14:53:30 +0200

    Update menuadministrative.py
    Update grâce au débug !
    
    Co-Authored-By: ldelpier <123209833+ldelpier@users.noreply.github.com>

--- src/menupersonnel/menupersonneladministratif/menuadministrative.py
+++ src/menupersonnel/menupersonneladministratif/menuadministrative.py
@@ -49,11 +49,11 @@ def main_persoadmin_menu (db: DataBase, customer_id):
     if len(organe_choice) != 0 :
         while True :
             date_choice = get_date("Enter a date for your operation")
-            db.execute("SELECT T.date FROM TRANPLANTATION T;")
+            db.execute("SELECT T.date_ FROM TRANSPLANTATION T;")
             date_table = db.table 
 
             if date_choice is not date_table :
-                print("Your operation will attend on %d", date_choice)
+                print("Your operation will attend on", date_choice)
                 break
 
             else :
@@ -65,22 +65,22 @@ def main_persoadmin_menu (db: DataBase, customer_id):
     if len(organe_choice) != 0 :
         print("We will assign you a doctor, an anaesthetist and a nurse")
         #To get the doctors who are free
-        db.execute("SELECT T.id, D.inami_number, D.D_w_id, D.id FROM TRANSPLANTATION T, DOCTOR D WHERE T.D_w_id <> D.id;")
+        db.execute("SELECT T.id, D.inami_number, T.D_w_id, D.id FROM TRANSPLANTATION T, DOCTOR D WHERE T.D_w_id <> D.id;")
         doctor_choice = db.table # récupère le résultat de la requête
         if len(doctor_choice) == 0 :
             print("All of the medecins is occuped")
         else : 
-            doc_id = doctor_choice[0][3] # get the doc inami from the result
+            doc_id = doctor_choice[0][1] # get the doc inami from the result
             print ("Your medecin is %i", doc_id)
 
 
         #To get the anesthesists who are free
-        db.execute("SELECT T.id, T.A_w_id, A.inami_number, A.id FROM TRANSPLANTATION T, ANESTHESIST A WHERE T.A_w_id <> A.id;")
+        db.execute("SELECT T.id, A.inami_number, T.A_w_id, A.id FROM TRANSPLANTATION T, ANAESTHESIST A WHERE T.A_w_id <> A.id;")
         anesthesist_choice = db.table 
         if len(anesthesist_choice) == 0 :
             print("All of the anesthesist is occuped")
         else : 
-            anesthesist_id = anesthesist_choice[0][3] # get the anesthesist inami from the result
+            anesthesist_id = anesthesist_choice[0][1] # get the anesthesist inami from the result
             print ("Your anesthesist is %i", anesthesist_id)
 
         
@@ -97,39 +97,42 @@ def main_persoadmin_menu (db: DataBase, customer_id):
                 nurse2_id = nurse_choice[1][2]
                 print ("Your nurse is %i", nurse2_id)
 
+        if len(doctor_choice) != 0 and len(anesthesist_choice) != 0 and len(nurse_choice) != 0 : 
+            #To get the price of the price of the organe
+            organe_price = ORGAN_DICO[organe_type_choice][0]
 
-        #To get the price of the price of the organe
-        organe_price = ORGAN_DICO[organe_choice][0]
+            #To get the price of the pockets of blood
+            nbr_poche500 = ORGAN_DICO[organe_type_choice][1] 
+            prix_500 = nbr_poche500*BLOODPOCHE
+            nbr_poche480 = ORGAN_DICO[organe_type_choice][2] 
+            prix_480 = nbr_poche480*BLOODPOCHE
+            nbr_poche450 = ORGAN_DICO[organe_type_choice][3]
+            prix_450 = nbr_poche450*BLOODPOCHE
 
-        #To get the price of the pockets of blood
-        nbr_poche500 = ORGAN_DICO[organe_choice][1] 
-        prix_500 = nbr_poche500*BLOODPOCHE
-        nbr_poche480 = ORGAN_DICO[organe_choice][2] 
-        prix_480 = nbr_poche480*BLOODPOCHE
-        nbr_poche450 = ORGAN_DICO[organe_choice][3]
-        prix_450 = nbr_poche450*BLOODPOCHE
+            prix_totalblood = prix_450 + prix_480 + prix_500 
 
-        prix_totalblood = prix_450 + prix_480 + prix_500 
+            #To get the salary of the staff
+            doctor_salary = SALARY_DOCTOR_TRANSPL[organe_type_choice]
+            anesthesist_salary = SALARY_ANESTHESIST_TRANSPL[organe_type_choice]
+            if nursenbr_choice == 1 :
+                nurse_salary = SALARY_NURSE_TRANSPL[organe_type_choice]*2
+            else :
+                nurse_salary = SALARY_NURSE_TRANSPL[organe_type_choice]
 
-        #To get the salary of the staff
-        doctor_salary = SALARY_DOCTOR_TRANSPL[organe_choice]
-        anesthesist_salary = SALARY_ANESTHESIST_TRANSPL[organe_choice]
-        if nursenbr_choice == 1 :
-            nurse_salary = SALARY_NURSE_TRANSPL[organe_choice]*2
-        else :
-            nurse_salary = SALARY_NURSE_TRANSPL[organe_choice]
+            salary_total = doctor_salary + anesthesist_salary + nurse_salary
+            
+            #To get the price of the transplantation
+            transplantation_price = organe_price + prix_totalblood + salary_total 
 
-        salary_total = doctor_salary + anesthesist_salary + nurse_salary
+            #Insert in the table TRANSPLANTATION
+            insert_into(
+                database=db,
+                table="TRANSPLANTATION",
+                attributes=("date_", "id", "Con_id", "price", "Rec_id", "D_w_id", "A_w_id"),
+                values=(date_choice, id, organe_id, transplantation_price, customer_id, doc_id, anesthesist_id) 
+            )
         
-        #To get the price of the transplantation
-        transplantation_price = organe_price + prix_totalblood + salary_total 
-
-        #Insert in the table TRANSPLANTATION
-        insert_into(
-            database=db,
-            table="TRANSPLANTATION",
-            attributes=("date_", "id", "Con_id", "price", "Rec_id", "D_w_id", "A_w_id"),
-            values=(date_choice, id, organe_id, transplantation_price, customer_id, doc_id, anesthesist_id) 
-        )
+        else :
+            print("Unfortunately, nobody is available for your transplantation so comme back later")
 
 
