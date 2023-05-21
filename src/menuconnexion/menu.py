8c896854fd05161c6fb72543ae02641d55ef2b3a
iDraKzy <57191896+iDraKzy@users.noreply.github.com>
2023-05-21 15:43:19 +0200

    french -> english

--- src/menuconnexion/menu.py
+++ src/menuconnexion/menu.py
@@ -22,9 +22,9 @@ def main_login_menu(database: DataBase):
     -----
         database (DataBase): Data base connected for this user (the accountent)
     """
-    print("Bienvenue chez Adopte ton mort. Que souhaitez vous faire ?")
-    print("1. Se connecter")
-    print("2. S'inscire")
+    print("Welcome to Adopte ton mort. What do you wish to do?")
+    print("1. Login")
+    print("2. Register")
 
     input_valid = False
     while not input_valid:
@@ -36,10 +36,10 @@ def main_login_menu(database: DataBase):
         elif choice == 2:
             input_valid = True
             register_action(database)
-            print("Inscription enregistrée. Veuillez vous connecter.")
+            print("Registration confirmed. Please login.")
             user = _try_login(database)
         else:
-            print("Votre sélection n'est pas valide, réessayez.")
+            print("Your selection is invalid, try again.")
     if user != None:
         logged_login_menu(database, user)
 
@@ -50,7 +50,7 @@ def _try_login(database: DataBase) -> User:
         user = login_action(database)
         attempt += 1
         if attempt > 5:
-            print("Trop de tentative, fermeture.")
+            print("Too many tries, closing...")
             exit()
     return user
 
@@ -70,40 +70,40 @@ def logged_login_menu(db: DataBase, user: User):
     while True:
         is_customer = "CUSTOMER" in user.userGroup
         if is_customer:
-            print("1. Aller dans le menu client")
+            print("1. Got to the customer menu")
         else:
-            print("1. Créer un compte client")
+            print("1. Create a customer account")
 
         is_accountant = "ACCOUNTANT" in user.userGroup
         if is_accountant:
-            print("2. Rejoindre le menu comptable")
+            print("2. Go to the accountant's menu")
         
         is_nurse = "NURSE" in user.userGroup
         if is_nurse:
-            print("3. Rejoindre le menu des infirmières")
+            print("3. Go to the nurse's menu")
         
         is_doctor = "DOCTOR" in user.userGroup
         if is_doctor:
-            print("4. Rejoindre le menu des médecins")
+            print("4. Go to the doctor's menu")
         
         is_anaesthesist = "ANAESTHESIST" in user.userGroup
         if is_anaesthesist:
-            print("5. Rejoindre le menu des anesthésistes")
+            print("5. Go to the anaesthesiologist's menu")
         
         is_hr = "HR" in user.userGroup
         if is_hr:
-            print("6. Rejoindre le menu RH")
+            print("6. Go to the hr's menu")
 
         is_ceo = "CEO" in user.userGroup
         if is_ceo:
-            print("7. Rejoindre le menu PDG")
+            print("7. Go to the CEO's menu")
 
         is_administrative_personnal = "STAFF" in user.userGroup
         if is_administrative_personnal:
-            print("8. Rejoindre le menu personnel administratif")
+            print("8. Go to the staff's menu")
 
-        user_choice = get_int("Entrer le chiffre correspondant au menu que vous souhaitez")
-        error_message = "Vous n'avez pas les permissions pour aller dans ce menu. Réessayez."
+        user_choice = get_int("Enter the number corresponding to what you want to do.")
+        error_message = "You don't have sufficient privileges to go to this menu. Please try again."
         if user_choice == 1:
             if is_customer:
                 main_menu_customer(db, user)
